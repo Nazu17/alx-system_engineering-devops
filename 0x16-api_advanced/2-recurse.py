fce378ea58cc1f689003fcdb3e19a12a814aa842
Nazrawi Asrat <nasrat2676@gmail.com>
2024-03-05 12:12:40 -0800

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Recursive module to querry reddit api"""
+import requests
+import sys
+
+
+def add_title(hot_list, hot_posts):
+    """prints a sorted count of given keywords
+
+    Args:
+        subreddit: strn
+        hot_list: list
+        count: int
+
+    Returns:
+        recursively a list containing the titles of all hot articles
+    """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
